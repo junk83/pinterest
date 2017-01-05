dbda9a0bcca73b20dfed541f5a91b977e7e1a0a3
junk83 <junkimura83@gmail.com>
2017-01-05 13:44:22 +0900

    customize devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+
+  devise_scope :user do
+    post 'users/sign_up/register' => 'users/registrations#register'
+  end
   root 'pins#index'
 end
