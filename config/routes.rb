476b61efa269171b58a8bf713416cd4fe4df0e4e
junk83 <junkimura83@gmail.com>
2017-01-18 13:25:21 +0900

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: { registrations: 'users/registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
 
   devise_scope :user do
     post 'users/sign_up/register' => 'users/registrations#register'
   end
+
+  resources :users, only:[:show, :edit, :update]
   root 'pins#index'
 end
