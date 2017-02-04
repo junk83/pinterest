660828197575fe76f122b7cfd0da3b0209c51d98
junk83 <junkimura83@gmail.com>
2017-02-04 18:10:11 +0900

    edit route

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,23 @@ Rails.application.routes.draw do
   end
 
   resources :users, only:[:show, :edit, :update] do
+    resource :follow_users, only: [:create, :destroy]
+    member do
+      get :boards, :pins
+      get :following, :followers
+    end
     collection do
       patch 'update_password'
     end
   end
+
+  post '/pins/upload', to: 'pins#upload'
+
+  resources :boards do
+    resources :pins
+  end
+
+
   root 'pins#index'
+
 end
