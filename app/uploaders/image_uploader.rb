9a0b40f58fe776bffa9201124efcf177b5447211
junk83 <junkimura83@gmail.com>
2017-03-02 21:15:58 +0900

    ストレージをs3に変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,20 +4,26 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   include CarrierWave::RMagick
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # 保存先を切り替え
+  case Rails.env
+    when 'production'
+      storage :fog
+    when 'development'
+      storage :fog
+    when 'test'
+      storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}"
+    "#{Rails.env}/uploads/#{model.class.to_s.underscore}/#{mounted_as}"
     # /#{model.id}"
   end
 
-  def cache_dir
-    "uploads/tmp/pin"
-  end
+  # def cache_dir
+  #   "/uploads/tmp/pin"
+  # end
 
   # 画像は300pxにリサイズ
   process :resize_to_limit => [236, nil]
